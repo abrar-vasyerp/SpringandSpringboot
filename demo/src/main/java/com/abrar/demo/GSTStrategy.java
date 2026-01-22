201db5e34c2704c0be572b24cd44d7f23bc085ff
abrar-vasyerp <abrarchhapavala@vasyerp.com>
2026-01-22 14:46:01 +0530

    application.property+application.yaml

--- demo/src/main/java/com/abrar/demo/GSTStrategy.java
+++ demo/src/main/java/com/abrar/demo/GSTStrategy.java
@@ -1,11 +1,23 @@
 package com.abrar.demo;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component("gstStrategy")
+@ConfigurationProperties(prefix = "billing.gst")
 public class GSTStrategy implements TaxStrategy {
+    private double rate;
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
     @Override
     public void applyTax() {
-        System.out.println("GST is applied...");
+        System.out.println("GST is applied..."+rate);
     }
 }
--- demo/src/main/java/com/abrar/demo/IGSTStrategy.java
+++ demo/src/main/java/com/abrar/demo/IGSTStrategy.java
@@ -1,11 +1,23 @@
 package com.abrar.demo;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component("igstStrategy")
+@ConfigurationProperties("billing.igst")
 public class IGSTStrategy implements TaxStrategy {
+    private double rate;
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
     @Override
     public void applyTax() {
-        System.out.println("Inter State GST is applied...");
+        System.out.println("Inter State GST is applied... "+rate);
     }
 }
